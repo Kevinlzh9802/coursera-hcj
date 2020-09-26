e446c4ff20c1ba2000cd66c32eac9de2da7c7df5
Zonghuan Li <kevinli606Wgmail.com>
2020-09-26 23:15:39 +0800

    switch to bootstrap v4.5

---
+++ site/js/string-generate.js
@@ -0,0 +1,5 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+  console.log("111");
+  var stringGeneratorOptions = $("#stringGeneratorForm")[0].numbersEligible1;
+  console.log(stringGeneratorOptions);
+});
